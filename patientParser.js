4c162a2972da88581eaafdc9349ab57ad8566ad1
federicaCapobianco <capobianco.fede01@gmail.com>
2023-11-21 14:36:33 +0100

    add - function

--- patientParser.js
+++ patientParser.js
@@ -1,1 +1,15 @@
-console.log("hello")
+//load the patients.csv file
+var fs = require('fs');
+var parse = require('csv-parse');
+
+//create the parser
+var parser = parse({delimiter: ','}, function(err, data){
+  async.eachSeries(data, function(line, callback){
+    //do something with the line
+    createPatient(line, callback);
+  });
+});
+
+function createPatient(line,callback){
+
+}
